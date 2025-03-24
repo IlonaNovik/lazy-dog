b6e0d6a0da4206685720b902f061090a75a580cc
poulou0 <poulou.0@gmail.com>
2025-03-24 23:57:13 +0100

    integrate with 11labs

--- app/chaturanga/tutorial/page.tsx
+++ app/chaturanga/tutorial/page.tsx
@@ -4,29 +4,33 @@ import { useEffect, useRef, useState } from "react";
 import { DogIcon } from "lucide-react";
 import { Avatar } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-
-const messages = [
-  { text: "Hi there!", sender: "left" },
-  { text: "Hello! How can I help you today?", sender: "right" },
-  { text: "I need some information about your services.", sender: "left" },
-  { text: "Sure, I’d be happy to help with that.", sender: "right" },
-];
+import { useConversation } from "@11labs/react";
 
 export default function Chaturanga() {
   const router = useRouter();
-  const [visibleMessages, setVisibleMessages] = useState<typeof messages>([]);
+  const [visibleMessages, setVisibleMessages] = useState<{ source: "ai" | "user", message: string }[]>([]);
   const isFirstRender = useRef(true);
+  const chatRef = useRef<HTMLDivElement | null>(null);
+
+  const { startSession } = useConversation({
+    agentId: "Z32ia3snE6dapfxi3aaM",
+    onDisconnect: () => router.push("/chaturanga"),
+    onMessage: (msg) => {
+      setVisibleMessages((prev) => [...prev, msg])
+    },
+  });
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [visibleMessages]);
 
   useEffect(() => {
     if (isFirstRender.current) {
-      isFirstRender.current = false;
-      messages.forEach((msg, index) => {
-        setTimeout(() => {
-          setVisibleMessages((prev) => [...prev, msg]);
-        }, index * 1000);
-      });
+      isFirstRender.current = false
+      startSession();
     }
   }, []);
 
@@ -35,46 +39,38 @@ export default function Chaturanga() {
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-yoga-earth leading-tight">
         Let's talk about it
       </h1>
-      <Card className="flex flex-col p-4 m-4 min-w-96 mx-auto">
-        {visibleMessages.map((msg, index) => (
-          <div
-            key={index}
-            className={`relative max-w-xs p-3 my-2 rounded-lg duration-500 animate-float repeat-1 ${
-              msg.sender === "left"
+      <Card
+        className="p-4 max-h-96 overflow-auto mx-auto"
+        ref={chatRef}
+      >
+        <div className="flex flex-col min-w-96 mx-16">
+          {visibleMessages.map((msg, index) => (
+            <div
+              key={index}
+              className={`relative max-w-xs p-3 my-2 rounded-lg duration-500 animate-float repeat-1 ${msg.source === "user"
                 ? "bg-gray-300 self-start"
                 : "bg-blue-500 text-white self-end"
-            }`}
-          >
-            {msg.text}
-            <div
-              className={`absolute w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent bottom-2 ${
-                msg.sender === "left"
+                }`}
+            >
+              {msg.message}
+              <div
+                className={`absolute w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent bottom-2 ${msg.source === "user"
                   ? "border-r-8 border-r-gray-300 left-0 -ml-2"
                   : "border-l-8 border-l-blue-500 right-0 -mr-2"
-              }`}
-            ></div>
-            {index === visibleMessages.length - 1 && (
-              <Avatar
-                className={`absolute border-2 text-black flex justify-center items-center ${
-                  msg.sender === "left" ? "left-0 -ml-16" : "right-0 -mr-16"
-                } bottom-0`}
-              >
-                {msg.sender === "left" ? "You" : <DogIcon className="size-9" />}
-              </Avatar>
-            )}
-          </div>
-        ))}
+                  }`}
+              ></div>
+              {index === visibleMessages.length - 1 && (
+                <Avatar
+                  className={`absolute border-2 text-black flex justify-center items-center ${msg.source === "user" ? "left-0 -ml-16" : "right-0 -mr-16"
+                    } bottom-0`}
+                >
+                  {msg.source === "user" ? "You" : <DogIcon className="size-9" />}
+                </Avatar>
+              )}
+            </div>
+          ))}
+        </div>
       </Card>
-      <Button
-        className={`transition-opacity duration-700 delay-700 ${
-          visibleMessages.length === messages.length
-            ? "opacity-100"
-            : "opacity-0"
-        }`}
-        onClick={() => router.push("/chaturanga")}
-      >
-        Go to the lesson (Automatically from the onEndSession callback)
-      </Button>
     </div>
   );
 }
