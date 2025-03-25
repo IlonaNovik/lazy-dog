474f1a2a16f7f1cf2f574d47b5bc7716c69ee5a2
Chayanika Khatua <chayanika.khatua@ing.com>
2025-03-25 12:02:59 +0100

    chore: add pose feedback page

---
+++ app/pose-feedback/page.tsx
@@ -0,0 +1,5 @@
+export default function PoseFeedback() {
+  return <div className="bg-yoga-sand/30 h-screen w-screen flex flex-col items-center justify-center">
+    <iframe src="http://localhost:3001" className="h-screen w-screen flex flex-col items-center justify-center"></iframe>
+  </div>
+}
