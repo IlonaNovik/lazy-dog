ae465852d14cc2328aadd674a93055e10cac1387
Chayanika Khatua <chayanika.khatua@ing.com>
2025-03-25 12:21:40 +0100

    chore: small cleanup

--- app/pose-feedback/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function PoseFeedback() {
-  return <div className="bg-yoga-sand/30 h-screen w-screen flex flex-col items-center justify-center">
-    <iframe src="http://localhost:3001" className="h-screen w-screen flex flex-col items-center justify-center"></iframe>
-  </div>
-}
