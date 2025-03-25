d6973bb1e747e76b28abbbf6d0c74c56e049ce6a
Chayanika Khatua <chayanika.khatua@ing.com>
2025-03-25 17:15:13 +0100

    chore: center align

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -17,9 +17,10 @@ export function LandingHero() {
       </div>
 
       <div className="container relative">
-        <div className="grid md:grid-cols-12 gap-4 items-center">
-          <div className="md:col-span-6 lg:col-span-5 space-y-8 relative z-10">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-yoga-earth leading-tight mb-8">
+        <div className="flex w-full gap-4 items-center">
+          <div className="mx-auto m-6 max-w-[600px]">
+            <div className="space-y-8 w-full">
+               <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-yoga-earth leading-tight mb-8">
               Exercises
             </h1>
             <Link href="/chaturanga/tutorial">
@@ -55,6 +56,8 @@ export function LandingHero() {
               color="dusk"
             />
           </div>
+            </div>
+           
         </div>
       </div>
     </section>
